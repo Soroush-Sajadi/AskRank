7e726678a3dfb1ec12f6b1f0982b116f5ad07446
SoroushSajadi <soroush.sajadi@kaptena.se>
2023-02-03 14:45:50 +0100

    mock data

--- Service/src/Contorller/router.ts
+++ Service/src/Contorller/router.ts
@@ -1,17 +1,10 @@
 import express, { Express, Request, Response } from 'express';
-import axios from 'axios'
 import dotenv from 'dotenv';
 dotenv.config();
-
 const router: Express = express();
-const port:string = process.env.DEV_PORT
 
 router.get('/', (req: Request , res: Response) => {
   res.send('Payload router node express');
-});
-
-router.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
 
 export default router
