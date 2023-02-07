385474ddfdfe42bf7747a2dd00c6778ad039d461
SoroushSajadi <soroush.sajadi@kaptena.se>
2023-02-07 16:22:19 +0100

    tailwind is removed

--- App/src/src/Header/index.tsx
+++ App/src/src/Header/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
+import './header.css'
 
 import { HeaderNavigation, AppName } from '../Strings'
 const Header = () => {
@@ -9,13 +10,13 @@ const Header = () => {
   }
   return (
     <div>
-      <div className="flex justify-between pt-[20px] px-[100px]">
-        <span className="text-[30px]">{AppName}</span>
-        <div className="space-x-[30px]">
-          <span className="text-[20px] cursor-pointer" onClick={() => onNavigate(HeaderNavigation.Home)}>{HeaderNavigation.Home}</span>
-          <span className="text-[20px] cursor-pointer" onClick={() => onNavigate(HeaderNavigation.About)}>{HeaderNavigation.About}</span>
-          <span className="text-[20px] cursor-pointer" onClick={() => onNavigate(HeaderNavigation.Contacts)}>{HeaderNavigation.Contacts}</span>
-          <span className="text-[20px] cursor-pointer" onClick={() => onNavigate(HeaderNavigation.Language)}>{HeaderNavigation.Language}</span>
+      <div className="Header">
+        <span className="HeaderAppName">{AppName}</span>
+        <div className="HeaderIcons">
+          <span onClick={() => ""}>{HeaderNavigation.Home}</span>
+          <span onClick={() => onNavigate(HeaderNavigation.About)}>{HeaderNavigation.About}</span>
+          <span onClick={() => onNavigate(HeaderNavigation.Contacts)}>{HeaderNavigation.Contacts}</span>
+          <span onClick={() => onNavigate(HeaderNavigation.Language)}>{HeaderNavigation.Language}</span>
         </div>
       </div>
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
