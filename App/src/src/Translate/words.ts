d032fcf8efa914f33c00e8c2cd7c4b0bee0c9e4a
SoroushSajadi <soroush.sajadi@kaptena.se>
2023-02-28 15:39:09 +0100

    initial sort

--- App/src/src/Translate/words.ts
+++ App/src/src/Translate/words.ts
@@ -1,10 +1,12 @@
 export const words: any = {
-  'amount': '0013',
-  'populationYearFifty': '0012',
+  'medianAge': '0004',
   'population': '0005',
+  'fertilityRate': '0006',
   'male': '0009',
   'female': '0010',
   'total': '0011',
   'today': '0012',
   'fertilityRateYearFifty': '0012',
+  'amount': '0013',
+  'populationYearFifty': '0012',
 }
--- Service/src/ControllerUtils/countriesUtils.ts
+++ Service/src/ControllerUtils/countriesUtils.ts
@@ -1,15 +1,32 @@
 import { mockDB } from '../Mocks/mockDB'
 import { Country } from '../Models/Country'
-export const getCountriesDataForRef = (ref: string) => {
-  const  countries  = mockDB.countries
-  return countries.map((country: Country ) => {
+import { COUNTRY_PROPERTIES } from './constants'
+import sortBy from 'lodash/sortBy'
+
+
+export const getCountriesDataForRef = (subTopic: string) => {
+  const countries  = mockDB.countries
+  const soretedCountries = getCountriesSorted(countries, subTopic)
+  return soretedCountries.map((country: Country ) => {
     return {
       id: country.id,
       name: country.name,
       continent: country.continent,
       yearEstimation: country.yearEstimation,
-      [ref]: country[ref as keyof Country]
+      code: country.code,
+      [subTopic]: country[subTopic as keyof Country]
     }
   })
 }
 
+const getCountriesSorted = (countries: Country[], subTopic: string) => {
+  if(subTopic === COUNTRY_PROPERTIES.POPULATION) {
+    return sortBy(countries, (country) => country.population.amount).reverse()
+  }
+  if(subTopic === COUNTRY_PROPERTIES.MEDIAN_AGE) {
+    return sortBy(countries, (country) => country.medianAge.total).reverse()
+  }
+  if(subTopic === COUNTRY_PROPERTIES.FERTILITY_RATE) {
+    return sortBy(countries, (country) => country.fertilityRate.today).reverse()
+  }
+}
